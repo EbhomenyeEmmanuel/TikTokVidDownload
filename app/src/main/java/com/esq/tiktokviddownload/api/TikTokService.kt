4a3df53b7057d7e36505f62ae136a5d2fa708389
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2020-08-04 03:20:19 +0100

    Added a logging interceptor to retrofit

--- app/src/main/java/com/esq/tiktokviddownload/api/TikTokService.kt
+++ app/src/main/java/com/esq/tiktokviddownload/api/TikTokService.kt
@@ -2,6 +2,7 @@ package com.esq.tiktokviddownload.api
 
 import com.esq.tiktokviddownload.model.TikTokResponse
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -17,13 +18,17 @@ interface TikTokService {
         @Query("url") url: String
     ): TikTokResponse
 
-
     companion object {
-        private val httpClient = OkHttpClient.Builder()
-            .callTimeout(1, TimeUnit.MINUTES)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(15, TimeUnit.SECONDS)
+        private val logInterceptor = HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+        private val httpClient = OkHttpClient.Builder().apply {
+            callTimeout(1, TimeUnit.MINUTES)
+            connectTimeout(30, TimeUnit.SECONDS)
+            readTimeout(30, TimeUnit.SECONDS)
+            writeTimeout(15, TimeUnit.SECONDS)
+            addInterceptor(logInterceptor)
+        }
         private var retrofit: Retrofit? = null
 
         val client: TikTokService
